163490d3d3f9b062a15dffe3fdf70820259282a5
Sandip Deb <62937399+debsandipagt@users.noreply.github.com>
2026-04-28 06:00:47 +0000

    Model path changes from local to github codespace in load_model.py file

--- model/load_model.py
+++ model/load_model.py
@@ -3,6 +3,7 @@
 # 📦 IMPORT LIBRARIES
 # ================================
 import pickle                                   # WHY: Load trained ML model
+import os
 
 
 # ================================
@@ -10,7 +11,8 @@ import pickle                                   # WHY: Load trained ML model
 # ================================
 
 def load_model():
-    with open(r"E:\Tavishi_Mentorship_Program\19_CampusX_FasiAPI_and_pydantic-tutorial\Fast_api_project\model\model.pkl", "rb") as f:
+    model_path = os.path.join(os.path.dirname(__file__), "model.pkl")
+    with open(model_path, "rb") as f:
         model = pickle.load(f)
         return model
     # WHY: Load trained ML model once at startup → avoids reloading per request (efficient)
